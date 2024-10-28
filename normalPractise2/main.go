6d46e12b86e8c4650c979f2ebe0e1dc54f45d126
Eniitan <enitanolawale0@gmail.com>
2024-10-28 02:19:20 +0100

    progress

--- normalPractise2/main.go
+++ normalPractise2/main.go
@@ -1,2 +1,30 @@
 package main
 
+import (
+	"fmt"
+)
+
+// var someNums = []int {1,2,3,4,5,6,7,8,9}
+
+func main(){
+	fmt.Println("we up!!!")
+
+	// for index, value := range someNums {
+	// 	fmt.Println(index, value)
+	// }
+
+	myMap := map[string]string {
+		"name": "Ronaldo",
+		"club": "Al Nassr",
+		"age": "39",
+		"height": "6'2",
+	}
+
+	myMap["bestie"] = "Adebayor"
+
+	fmt.Println(myMap, myMap["name"])
+
+	_, present := myMap["width"]
+
+	fmt.Println(present)
+}
